203f213d2bc5db67dd7b6bf2ce5bf2ba7e14a4ce
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2022-11-19 09:26:57 -0500

    refact: use nx

--- packages/ngrx-firebase/src/lib/ngrx-firebase.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ngrxFirebase } from './ngrx-firebase';
-
-describe('ngrxFirebase', () => {
-  it('should work', () => {
-    expect(ngrxFirebase()).toEqual('ngrx-firebase');
-  });
-});
--- packages/ngrx-firebase/src/lib/ngrx-firebase.ts
+++
@@ -1,3 +0,0 @@
-export function ngrxFirebase(): string {
-  return 'ngrx-firebase';
-}
