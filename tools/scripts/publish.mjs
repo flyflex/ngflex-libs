c81e11e5ca0ccc343c0305ce56403cd76514263d
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2023-05-12 10:13:08 -0400

    chore: update dependencies and sync versions

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -11,7 +11,7 @@ import nrwlDevkit from '@nrwl/devkit';
 const { readCachedProjectGraph } = nrwlDevkit;
 
 import { execSync } from 'child_process';
-import { copyFileSync, readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import chalk from 'chalk';
 
 function invariant(condition, message) {
@@ -24,20 +24,15 @@ function invariant(condition, message) {
 const rootPath = process.cwd();
 const yarnRcPath = `${rootPath}/.yarnrc.yml`;
 
+const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
+
 // Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
 // Default "tag" to "next" so we won't publish the "latest" tag by accident.
 const [, , name, version, tag = 'next', otp = null] = process.argv;
 
 const usePackageJsonVersion = version === 'readFromPackageJson';
+const useGlobalVersion = !!version || version === 'useGlobalVersion';
 
-// A simple SemVer validation to validate the version
-if (!usePackageJsonVersion) {
-  const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
-  invariant(
-    version && validVersion.test(version),
-    `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
-  );
-}
 const graph = readCachedProjectGraph();
 const project = graph.nodes[name];
 
@@ -46,21 +41,53 @@ invariant(
   `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`
 );
 
-const outputPath = project.data?.targets?.build?.options?.outputPath;
+const outputPath = project.data?.targets?.build?.outputPath;
 invariant(
   outputPath,
-  `Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`
+  `Could not find "build.outputs" of project "${name}". Is project.json configured correctly?`
 );
 
+// May be needed for yarn authentication on a private repo
 // copyFileSync(yarnRcPath, `${outputPath}/.yarnrc.yml`);
+
+
+const { version: globalJsonVersion } = JSON.parse(readFileSync(`package.json`).toString());
+
+if (useGlobalVersion) {
+  invariant(
+    globalJsonVersion && globalJsonVersion !== '0.0.0' && validVersion.test(globalJsonVersion),
+    `No global version provided in root package.json or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${globalJsonVersion}.`
+  );
+}
 
 process.chdir(outputPath);
 
+const { version: distJsonVersion } = JSON.parse(readFileSync(`package.json`).toString());
+
+if (usePackageJsonVersion) {
+  invariant(
+    globalJsonVersion && globalJsonVersion !== '0.0.0' && validVersion.test(globalJsonVersion),
+    `No local version provided in root package.json or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${globalJsonVersion}.`
+  );
+}
+
+const versionToUse = useGlobalVersion
+  ? globalJsonVersion
+  : usePackageJsonVersion
+    ? distJsonVersion
+    : version;
+
+invariant(
+  versionToUse && versionToUse !== '0.0.0' && versionToUse !== 'VERSION' && validVersion.test(versionToUse),
+  `No target version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${globalJsonVersion}.`
+);
+
 if (!usePackageJsonVersion) {
-  // Updating the version in "package.json" before publishing
+  // Updating the version in package dist "package.json" before publishing
+
   try {
     const json = JSON.parse(readFileSync(`package.json`).toString());
-    json.version = version;
+    json.version = versionToUse;
     writeFileSync(`package.json`, JSON.stringify(json, null, 2));
   } catch (e) {
     console.error(
